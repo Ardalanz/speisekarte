dcc01366cecc75096db4cc475c9c22c681dc57f3
Ardalanzareie77 <Ardalan.zareie77@gmail.com>
2023-05-24 23:25:42 +0200

    datenb,html,css

--- admin/index.php
+++ admin/index.php
@@ -5,7 +5,92 @@ ist_eingeloggt();
 
 include "kopf.php";
 ?>
-<h1>Rezepte Administrationsbereich</h1>
-<p>Willkommen im geheimen Admin-Bereich<p>
+
+
+
+
+    
+        <style>
+            @font-face {
+            font-family: MarckScripts;
+            src: url(../fonts/PTSerif-BoldItalic.ttf);
+            font-weight: bold;
+        }
+
+
+        h1,
+        table,
+        thead,
+        tbody,
+        tr,
+        td {
+
+            margin: 10px;
+            padding: 25px;
+            border-style: inset;
+            max-width: 100%;
+            height: 100%;
+            margin-inline: auto;
+            box-shadow: 1px 1px 1px 1px transparent;
+            border-radius: 1%;
+            text-decoration: none;
+
+        }
+
+
+        header {
+            margin: 10px;
+        }
+
+
+        body {
+            background: linear-gradient(55deg, #000, #000000, #00000025, #000000);
+            background-size: 400% 800%;
+            animation: gradient 30s ease infinite;
+            color: rgb(255, 255, 255);
+        }
+
+
+        h1 {
+            color: white;
+        }
+
+
+        p {
+            color: var(--dark-orange);
+            margin: 20px;
+            text-align: center;
+            text-decoration: underline;
+            box-shadow: 5px 5px 50px 5px var(--dark-orange);
+        }
+
+
+        nav {
+            margin: 10px;
+            padding: 10px;
+            color: var(--dark-orange);
+            margin: 20px;
+            text-align: center;
+            text-decoration: none;
+            box-shadow: 5px 5px 50px 5px var(--dark-orange);
+            width: 100%;
+            font-size: 3rem;
+        }
+
+
+        a:hover {
+            color: #ffffff;
+            box-shadow: 1px 1px 5px 1px var(--dark-orange);
+        }
+
+
+        </style>
+
+    <h1>Administrationsbereich</h1>
+    <p>Willkommen im geheimen Admin-Bereich<p>
+
+
+
+
 <?php
 include "fuss.php";
--- admin/kopf.php
+++ admin/kopf.php
@@ -7,18 +7,22 @@
     <title>Rezepteverwaltung</title>
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="index.php">Start</a></li>
-            <li><a href="zutaten_liste.php">Zutaten</a></li>
-            <li><a href="rezepte_liste.php">Rezepte</a></li>
-            <li><a href="logout.php">Ausloggen</a>
-                Eingeloggt als: <?php echo $_SESSION["benutzername"];?>
-            </li>
+
+    <link rel="stylesheet" href="../css/adminForm.css">
+
+        <nav>
+            <ul>
+                <li><a href="index.php">Start</a></li>
+                <li><a href="zutaten_liste.php">Zutaten</a></li>
+                <li><a href="rezepte_liste.php">Rezepte</a></li>
+                <li><a href="logout.php">Ausloggen</a>
+                <br>
+                    Eingeloggt als: <?php echo $_SESSION["benutzername"];?>
+                </li>
 
 
-        </ul>
-    </nav>
+            </ul>
+        </nav>
 
 
 
